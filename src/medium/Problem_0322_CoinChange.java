3c32f94d47fd60739feeeb9dfdb79b8a95a11cc9
yuhang003 <314837610@qq.com>
2023-12-15 17:25:32 +0800

    零钱兑换

---
+++ src/medium/Problem_0322_CoinChange.java
@@ -0,0 +1,25 @@
+package medium;
+
+import java.util.Arrays;
+
+/**
+ * 322. 零钱兑换
+ */
+public class Problem_0322_CoinChange {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                if (dp[j - coins[i]] != Integer.MAX_VALUE) {
+                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
