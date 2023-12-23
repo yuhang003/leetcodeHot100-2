4ce34cebeffa055f8354b7b4f8ebd130d5be79a2
yuhang003 <314837610@qq.com>
2023-12-23 16:44:38 +0800

    目标和

---
+++ src/medium/Problem_0494_FindTargetSumWays.java
@@ -0,0 +1,29 @@
+package medium;
+
+/**
+ * 494. 目标和
+ */
+public class Problem_0494_FindTargetSumWays {
+
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if (Math.abs(target) > sum) return 0;
+        if ((sum + target) % 2 == 1) return 0;
+        int bagSize = (sum + target) >> 1;
+
+        int[] dp = new int[bagSize + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = bagSize; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+
+        return dp[bagSize];
+    }
+}
